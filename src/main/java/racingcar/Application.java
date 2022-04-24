3830f8d5bf513f72d1b251c5aff1bb8e0d33bd8f
Minjae Kim <minjae.kim@mytrum.com>
2022-04-24 22:01:25 +0900

    refactor: 전체 클래스 구조 개편으로 인한 자동차 경주 기능 리펙토링

--- src/main/java/racingcar/Application.java
+++ src/main/java/racingcar/Application.java
@@ -4,7 +4,6 @@ import racingcar.feature.Game;
 
 public class Application {
     public static void main(String[] args) {
-        // TODO: 프로그램 구현
         Game racingCarGame = new Game();
         racingCarGame.startGame();
     }
